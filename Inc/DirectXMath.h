7ca2df2af6d6a0ca83743c007c20ceaba3e5ed25
Jialong Zhai <joshua_astray@qq.com>
2020-08-03 10:04:38 +0800

    make it compatible with non-Windows environments

--- Inc/DirectXMath.h
+++ Inc/DirectXMath.h
@@ -143,7 +143,21 @@
 #endif
 #endif // !_XM_NO_INTRINSICS_
 
+#ifdef _WIN32
 #include "sal.h"
+#else
+#define _In_reads_(x) 
+#define _Out_writes_(x) 
+#define _In_reads_bytes_(x) 
+#define _Out_writes_bytes_(x) 
+#define _Use_decl_annotations_ 
+#define _Analysis_assume_ 
+#define _In_  
+#define _Out_ 
+#define _Out_opt_ 
+#define _Success_(x) 
+#endif
+
 #include <assert.h>
 
 #pragma warning(push)
